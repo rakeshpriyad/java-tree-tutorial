a3cab02802f0fcb27048d4f962ee71082509d14b
rakeshpriyad <rakeshpriyad@gmail.com>
2018-01-27 13:14:30 +0530

    node

--- src/com/aayush/tree/postorder/iterative/traversal/Node.java
+++ src/com/aayush/tree/postorder/iterative/traversal/Node.java
@@ -1,9 +1,21 @@
 package com.aayush.tree.postorder.iterative.traversal;
-class Node {
-		int data;
-		Node left, right;
+// A java program for iterative postorder traversal using stack
+ 
+import java.util.ArrayList;
+import java.util.Stack;
+  
+// A binary tree node
+class Node 
+{
+    int data;
+    Node left, right;
+  
+    Node(int item) 
+    {
+        data = item;
+        left = right;
+    }
+}
+  
 
-		public Node(int data) {
-			this.data = data;
-		}
-	}
+  
